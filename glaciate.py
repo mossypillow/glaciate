a099a8cde258bdb70391ed92dc8f16078296770c
vagrant <vagrant@precise64.(none)>
2017-04-11 23:24:28 +0000

    adding README

--- glaciate.py
+++ glaciate.py
@@ -1,5 +1,19 @@
-#!/bin/python 
+#!/usr/local/lib/python2.7
 
 import boto3 
+import yml
+
+with open('config', 'r') as ymlfile: 
+     cfg = yml.load(ymlfile) 
+print(data)
+
+
+# load options from config file
+
+# check local file system for changes (new files or update files) 
+
+# upload new files to AWS glacier 
+
+# upload and replace files  
 
 

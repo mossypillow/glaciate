668eb317cd903c7e224fe809310cea45567f54ed
Alex <aprivee@gmail.com>
2017-04-17 16:09:34 -0700

    fixed glaciate.py

--- glaciate.py
+++ glaciate.py
@@ -1,19 +1,23 @@
 #!/usr/local/lib/python2.7
 
-import boto3 
-import yml
-
-with open('config', 'r') as ymlfile: 
-     cfg = yml.load(ymlfile) 
-print(data)
-
+import boto3
+import json
+import hasher
+from pprint import pprint
 
 # load options from config file
+with open('config.json') as data_file:
+    data = json.load(data_file)
+pprint(data)
 
-# check local file system for changes (new files or update files) 
-
-# upload new files to AWS glacier 
-
-# upload and replace files  
+# check local file system for changes (new files or update files)
+# keep human readible log of files sent to glacier
+# if fileisnew then:
 
 
+# hash files to be transferred keep
+# hasher(file)
+
+# upload new files to AWS glacier
+
+# upload and replace files

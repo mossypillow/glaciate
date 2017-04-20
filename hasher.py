cee31f8d4d1af318abb329f0d3588c9b4c842637
Alex <aprivee@gmail.com>
2017-04-19 17:36:17 -0700

    fixed hasher

--- hasher.py
+++ hasher.py
@@ -1,8 +1,12 @@
+import hashlib
 
-
-def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
+def hashing(file):
+    largefile = file
+    BLOCKSIZE = 65536
+    hashing = hashlib.md5()
+    with open(largefile, 'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hashing.update(buf)
+            buf = afile.read(BLOCKSIZE)
+    return hashing.hexdigest()

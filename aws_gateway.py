3f8eccff4a3111a4a57e183afc969c245a280e1f
Alex <aprivee@gmail.com>
2017-05-02 17:22:18 -0700

    removed test

--- aws_gateway.py
+++ aws_gateway.py
@@ -3,7 +3,7 @@
 import boto3, botocore
 
 def to_s3(payload):
-    ''' description: Upload a file to s3. 
+    ''' description: Upload a file to s3.
     '''
 # Module that sends information from hash file (payload) to s3
     try:
@@ -19,18 +19,17 @@ def to_s3(payload):
 
 def from_s3():
 # Module that receives information from s3 resources
+    ''' description: Downloads payload from s3.
+    '''
     try:
         s3 = boto3.resource('s3')
         data = open('tmp/cur_s3.json', 'w')
         s3.meta.client.download_fileobj('12345supertestbucket', 'tmp_payload.json', data)
-
     except botocore.exceptions.ClientError, fault:
         raise
     except Exception, fault:
         raise
 
-from_s3()
-print('done')
 
 
 

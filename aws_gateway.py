7445fd3950f506a67521852fd47ab8dbd95651dc
Alex <aprivee@gmail.com>
2017-05-02 17:18:36 -0700

    fixed upload / download functionality

---
+++ aws_gateway.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+import boto3, botocore
+
+def to_s3(payload):
+    ''' description: Upload a file to s3. 
+    '''
+# Module that sends information from hash file (payload) to s3
+    try:
+        s3 = boto3.resource('s3')
+        data = open('tmp/tmp_payload.json', 'rb')
+        s3.Bucket('12345supertestbucket').put_object(Key='tmp_payload.json', Body=data)
+        print(data)
+
+    except botocore.exceptions.ClientError, fault:
+        raise
+    except Exception, fault:
+        raise
+
+def from_s3():
+# Module that receives information from s3 resources
+    try:
+        s3 = boto3.resource('s3')
+        data = open('tmp/cur_s3.json', 'w')
+        s3.meta.client.download_fileobj('12345supertestbucket', 'tmp_payload.json', data)
+
+    except botocore.exceptions.ClientError, fault:
+        raise
+    except Exception, fault:
+        raise
+
+from_s3()
+print('done')
+
+
+
